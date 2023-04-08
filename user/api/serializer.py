a0930a45209910250740e3493730ab0b8bc4d1b1
cuzknothz <91480011+cuzknothz@users.noreply.github.com>
2023-04-08 12:58:45 +0700

    🥕

--- user/api/serializer.py
+++ user/api/serializer.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from ..models import User
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -25,3 +26,12 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
